a8ba84aef48bed5392869a5ee3860b383090312b
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-11-22 12:29:56 +0530

    Create if_else_test.sh

---
+++ shell_scripting/if_else_test.sh
@@ -0,0 +1,9 @@
+# Hello World Program in Bash Shell
+#!/bin/bash
+MY_SHELL="csh" #no spaces 
+if [ "$MY_SHELL"="bash" ] #spaces after sqaure brackets
+then
+echo "You seem to like the bash shell."
+else
+echo "You don't seem to like the bash shell."
+fi
