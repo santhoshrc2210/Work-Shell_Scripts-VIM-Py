1df0eac21cd068f9de7720279dfa18aa7b9e6449
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-11-22 12:59:46 +0530

    Update if_else_test.sh

--- shell_scripting/if_else_test.sh
+++ shell_scripting/if_else_test.sh
@@ -1,9 +1,10 @@
 # Hello World Program in Bash Shell
 #!/bin/bash
-MY_SHELL="csh" #no spaces 
-if [ "$MY_SHELL"="bash" ] #spaces after sqaure brackets
+MY_SHELL="dd"
+if [ ${MY_SHELL} = "csh" ] #spaces after sqaure brackets and the equal to sign, otherwise it becomes variable assignment
 then
-echo "You seem to like the bash shell."
-else
-echo "You don't seem to like the bash shell."
+ echo "You seem to like the csh shell" 
+elif [ ${MY_SHELL} = "bash" ]
+then
+ echo "You seem to like the bash shell"
 fi
