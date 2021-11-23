397aa562bfb036ac0e10f47f80db96f97f700f0b
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-11-23 10:23:00 +0530

    Create 3_ex_3.sh

---
+++ shell_scripting/3_ex_3.sh
@@ -0,0 +1,14 @@
+Write a script that executes the command "cat /etc/shadow". If the command returns a 0 exit status,
+report "Command succeeded" and exit with a 0 exit status. If the command returns a non-zero exit
+status, report "Command failed" and exit with a 1 exit status
+
+cat /etc/shadow
+RETURN_CODE="$?"
+if [ $RETURN_CODE -eq "0" ]
+then
+echo "Command succeeded"
+exit 0
+else
+echo "Command failed"
+exit 1
+fi
